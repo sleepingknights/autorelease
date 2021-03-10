9e279c7f8ce05afb951c8cf42f2f8a6a93372275
rng74 <ers98ers@gmail.com>
2021-03-10 15:48:28 +0600

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 hub checkout master
-VERSION_NAME=`grep -oP 'versionName "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle
-hub release create -a ./${APP_FOLDER}/build/outputs/apk/debug/*-debug.apk -p -m "${RELEASE_TITLE} - v${VERSION_NAME}" Pre-Release
+VERSION_NAME=$(grep -oP 'versionName "\K(.*?)(?=")' ./"${APP_FOLDER}"/build.gradle)
+hub release create -a ./"${APP_FOLDER}"/build/outputs/apk/debug/*-debug.apk -p -m "${RELEASE_TITLE} - v${VERSION_NAME}" Pre-Release
